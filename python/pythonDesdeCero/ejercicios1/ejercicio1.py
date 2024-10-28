24ae1bf23be9510ae67cdfb21c7db05e3bde17d7
David Beltrán <david161189@gmail.com>
2024-10-27 21:02:50 -0600

    Delete python directory

--- python/pythonDesdeCero/ejercicios1/ejercicio1.py
+++
@@ -1,8 +0,0 @@
-# 1. Escribe un programa que le pida su nombre al usuario y como salida
-# le diga cuantos caracteres tiene su nombre.
-# test
-nombre = input("Ingresa tu nombre: ")
-if " " in nombre:
-    nombre = nombre.replace(" ", "")
-numero_caracteres = len(nombre)
-print(f"Tu nombre tiene {numero_caracteres} caracteres")
